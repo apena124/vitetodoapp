670df3dbaf40710308d0dd18d6f6d66ddf9d4fc6
apena124 <73714064+apena124@users.noreply.github.com>
2022-02-02 13:36:36 -0600

    Fihished App

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  mode: "jit",
+  purge: ["./index.html", "./src/**/*.{vue, js, ts, jsx, tsx}"],
+
+  theme: {
+    extends: {},
+  },
+  variants: {
+    extends: {},
+  },
+  plugins: [],
+};
